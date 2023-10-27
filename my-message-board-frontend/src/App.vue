2f5fafc495c0c0eb3aa455cdb8d2e8a52706d9a7
Peggy <but5863841@gmail.com>
2023-10-27 16:49:12 +0800

    fix: :zap: 測試前後端連接

--- my-message-board-frontend/src/App.vue
+++ my-message-board-frontend/src/App.vue
@@ -1,20 +1,29 @@
 <template>
   <div id="app">
     <h1>留言板</h1>
-    <input v-model="newMessage" @keyup.enter="addMessage" placeholder="輸入留言">
+    姓名: <input v-model="newSender" placeholder="輸入姓名"><br/>
+    內容: <input v-model="newMessage" placeholder="輸入留言"><br/>
+    <button @click="addSenderAndMessage">送出</button>
     <ul>
-      <li v-for="message in messages" :key="message.id">{{ message.text }}</li>
+      <h1>留言列表</h1>
+      <li v-for="message in messages" :key="message.id">
+        <p>編號: {{ message.id }}</p>
+        <p>姓名: {{ message.sender }}</p>
+        <p>內容: {{ message.message }}</p>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
-import axios from 'axios'; // 导入 Axios 库
+import axios from 'axios';
 
 export default {
   data() {
     return {
+      newSender: '',
       newMessage: '',
+      senders: [],
       messages: [],
     };
   },
@@ -22,23 +31,27 @@ export default {
     this.loadMessages();
   },
   methods: {
-    addMessage() {
-      axios.post('http://localhost:10000/api/messages', { text: this.newMessage })
+    addSenderAndMessage() {
+      // 发送发送者和消息到后端
+      axios.post('/api/messages', { sender: this.newSender, message: this.newMessage })
         .then(response => {
+          this.newSender = '';
           this.newMessage = '';
           this.loadMessages();
         })
         .catch(error => {
-          console.error('发生错误：', error);
+          console.error('發生錯誤：', error);
         });
     },
+
     loadMessages() {
-      axios.get('http://localhost:10000/api/messages')
+      // 获取留言消息列表，包括发送者信息
+      axios.get('/api/messages')
         .then(response => {
           this.messages = response.data;
         })
         .catch(error => {
-          console.error('发生错误：', error);
+          console.error('發生錯誤：', error);
         });
     },
   },
--- server.js
+++ server.js
@@ -1,59 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const axios = require('axios');
-
 const app = express();
-const port = process.env.PORT || 10000;
+const PORT = 10000; // 选择适合您的端口
+const cors = require('cors');
 
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
 
+// 模拟存储留言的数组
 const messages = [];
 
-const mysql = require('mysql2');
-
-const connection = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'root',
-  database: 'test_1026',
-});
-
-connection.connect(err => {
-  if (err) {
-    console.error('無法連接到資料庫:', err);
-  } else {
-    console.log('成功連接資料庫!');
-  }
-});
-
-
-app.get('/api/messages', (req, res) => {
-  res.json(messages);
-});
-
 app.post('/api/messages', (req, res) => {
-  const newMessage = req.body;
+  const { sender, message } = req.body;
+  if (!sender || !message) {
+    return res.status(400).json({ error: '請提供發送者和消息' });
+  }
+
+  const newMessage = { id: messages.length + 1, sender, message };
   messages.push(newMessage);
+
+  // 这里应该将消息存储到数据库或其他持久性存储中
+
   res.json(newMessage);
 });
 
-app.get('/api/data', async (req, res) => {
-  try {
-    const response = await axios.get('http://localhost:10000/api/messages');
-    res.json(response.data);
-  } catch (error) {
-    if (error.response) {
-      res.status(error.response.status).json({ error: '請求失敗' });
-    } else if (error.request) {
-      res.status(500).json({ error: '沒有收到響應' });
-    } else {
-      res.status(500).json({ error: '請求錯誤' });
-    }
-  }
+app.get('/api/messages', (req, res) => {
+  // 返回已存储的留言
+  res.json(messages);
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`服务器正在监听端口 ${PORT}`);
 });
